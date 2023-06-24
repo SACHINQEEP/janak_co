d2e72e800ca482f5630ee8ff0282da2ccaeec8a6
SACHINQEEP <patelsachinsp269@gmail.com>
2023-06-25 01:06:05 +0530

    UI changes

--- src/features/landingPage.tsx
+++ src/features/landingPage.tsx
@@ -79,8 +79,8 @@ const LandingPage = () => {
                 </Grid >
 
                 {/* Section AboutUs */}
-                <Grid component='section' container sx={{}}>
-                    <Grid item xs={12} sm={6} lg={6} md={6}>
+                <Grid component='section' container sx={{ pr: { md: 2 }, minHeight: { xs: '90dvh' }, }}>
+                    <Grid item xs={12} sm={6} lg={6} md={6} sx={{ mt: { xs: 5 } }}>
                         <motion.div
                             initial={{ opacity: 0, x: -50 }} // Initial animation values
                             animate={{ opacity: 1, x: 0 }} // Animation values to transition to
@@ -96,11 +96,11 @@ const LandingPage = () => {
                             transition={{ duration: 1.5 }} // Transition configuration
                         >
                             <Typography variant="h3" color="#f5821f" sx={{
-                                fontSize: { xs: 25, sm: 25, md: 36, lg: 40 },
+                                fontSize: { xs: 30, md: 36, lg: 40 },
                             }}>
                                 About Us
                             </Typography>
-                            <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 14, sm: 16, md: 19 } }}>
+                            <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 19 } }}>
                                 Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape. With a team of passionate experts driven by innovation, trust, bold strategies and confidence. We are committed to delivering outstanding results and empowering our clients to surpass their goals
                             </Typography>
                             <Link href='#' sx={{ fontWeight: 'bold', fontSize: '16px' }}>Learn More<ArrowRightAltIcon /></Link>
@@ -109,7 +109,7 @@ const LandingPage = () => {
                 </Grid>
 
                 {/* Why Section */}
-                <Grid component='section' container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Grid component='section' container sx={{ pl: { md: 2 }, minHeight: { xs: '100dvh' }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <Grid item xs={12} sm={6} lg={6} md={6} sx={{ order: { xs: 2, sm: 1 } }}>
                         <motion.div
                             initial={{ opacity: 0, x: 50 }} // Initial animation values
@@ -117,18 +117,18 @@ const LandingPage = () => {
                             transition={{ duration: 1.5 }} // Transition configuration
                         >
                             <Typography variant="h3" color="#f5821f" sx={{
-                                fontSize: { xs: 25, sm: 25, md: 36, lg: 40 },
+                                fontSize: { xs: 28, md: 36, lg: 40 },
                             }}>
                                 Why chosse Janak & Co.?<br />
                                 Here what sets us apart
                             </Typography>
-                            <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 14, sm: 16, md: 19 } }}>
+                            <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 19 } }}>
                                 Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape. With a team of passionate experts driven by innovation, trust, bold strategies and confidence. We are committed to delivering outstanding results and empowering our clients to surpass their goals
                             </Typography>
                             <Link href='#' sx={{ fontWeight: 'bold', fontSize: '16px' }}>Learn More<ArrowRightAltIcon /></Link>
                         </motion.div>
                     </Grid>
-                    <Grid item xs={12} sm={6} lg={6} md={6} sx={{ alignSelf: 'center', order: { xs: 1, sm: 2 } }}>
+                    <Grid item xs={12} sm={6} lg={6} md={6} sx={{ order: { xs: 1, sm: 2 } }}>
                         <motion.div
                             initial={{ opacity: 0, x: -50 }} // Initial animation values
                             animate={{ opacity: 1, x: 0 }} // Animation values to transition to
