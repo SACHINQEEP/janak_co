8e312d135a3eb1b9722c5870fd0283992fa74b39
SACHINQEEP <patelsachinsp269@gmail.com>
2023-06-25 00:14:29 +0530

    Adding some other blogs post list

--- src/features/Blogs/blogs.component.tsx
+++ src/features/Blogs/blogs.component.tsx
@@ -2,27 +2,28 @@
 import blogTwo from '../../assets/blog-02.jpg'
 import blogThree from '../../assets/blog-03.jpg'
 import blogOne from '../../assets/blog.jpg'
+import blogForth from '../../assets/meeting.jpg'
 
 export const BlogsPostList = [
     {
-        title: 'What I Love about it: The Blonde Abroad',
+        title: 'What I Love about it',
         description: 'Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape.',
         imageLink: blogOne
     },
     {
-        title: 'Best UX Design Blogs to Follow This Year',
+        title: 'Best UX Design Blogs',
         description: 'Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape.',
         imageLink: blogTwo
     },
     {
-        title: 'The Ultimate Guide to Advertising in 2023',
+        title: 'The Ultimate Guide to Advertising',
         description: 'Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape.',
         imageLink: blogThree
     },
     {
-        title: 'The Ultimate Guide to Advertising in 2023',
+        title: 'The Ultimate Meeting',
         description: 'Welcome to janak & co.. Where dreams soor and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brnads to reach new heights of success in the everevolving online landscape.',
-        imageLink: blogThree
+        imageLink: blogForth
     },
 ]
 
