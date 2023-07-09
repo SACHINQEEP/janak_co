e1bb1fd558288372540e8cb722f2f4201f2722b6
SACHINQEEP <patelsachinsp269@gmail.com>
2023-07-09 17:27:14 +0530

    Animation added

--- src/features/AboutUs/aboutUs.component.tsx
+++ src/features/AboutUs/aboutUs.component.tsx
@@ -1,9 +1,12 @@
 import { Box, Grid, Container, CssBaseline, Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
-// import { motion } from "framer-motion"
 import aboutUsContent from '../../assets/AboutUsPage.svg'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import { Link } from 'react-router-dom';
+
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 interface Data {
     contentName: string
@@ -11,7 +14,23 @@ interface Data {
     image: any
 }
 
+const boxVariant = {
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, scale: 0 }
+};
+
 export const AboutUsPageComponent = ({ contentName, order, image }: Data) => {
+
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            control.start("visible");
+        } else {
+            control.start("hidden");
+        }
+    }, [control, inView]);
 
     const imageItemOrder = order ? { xs: 1, sm: 2, md: 2, lg: 2, xl: 2 } : { xs: 1, sm: 1, md: 1, lg: 1, xl: 1 };
     const ItemOrder = order ? { xs: 2, sm: 1, md: 1, lg: 1, xl: 1 } : { xs: 2, sm: 2, md: 2, lg: 2, xl: 2 };
@@ -19,36 +38,40 @@ export const AboutUsPageComponent = ({ contentName, order, image }: Data) => {
     return (
         <Grid component='section' container sx={{ pr: { md: 2 }, minHeight: { xs: '90dvh', md: '50dvh', lg: '50dvh', xl: '10dvh' }, }}>
             <Grid item xs={12} sm={6} lg={6} md={6} sx={{ mt: { xs: 5 }, order: imageItemOrder }}>
-                {/* <motion.div
-                    initial={{ opacity: 0, x: -50 }} // Initial animation values
-                    animate={{ opacity: 1, x: 0 }} // Animation values to transition to
-                    transition={{ duration: 1.5 }} // Transition configuration
-                > */}
+                <motion.div
+                    className="box"
+                    ref={ref}
+                    variants={boxVariant}
+                    initial="hidden"
+                    animate={control}
+                >
 
-                <img src={image} alt="aboutUs" />
-                {/* </motion.div> */}
+                    <img src={image} alt="aboutUs" />
+                </motion.div>
             </Grid>
             <Grid item xs={12} sm={6} lg={6} md={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', order: ItemOrder }}>
-                {/* <motion.div
-                    initial={{ opacity: 0, x: 50 }} // Initial animation values
-                    animate={{ opacity: 1, x: 0 }} // Animation values to transition to
-                    transition={{ duration: 1.5 }} // Transition configuration
-                > */}
-                <Typography variant="h3" color='#f58634' sx={{
-                    fontSize: { xs: 30, md: 36, lg: 40 },
-                }}>
-                    {contentName}
-                </Typography>
-                <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 18 }, fontWeight: 300 }}>
-                    Welcome to Janak & Co., where dreams soar and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brand to reach new heights of success in the ever-evolving online landscape. With a team of passionate experts driven by innovation, trust, bold strategies and confidence, we are committed to delivering outstanding results and empowering our clients to surpass their goals.
-                </Typography>
-                {order === false
-                    ? <Link style={{ fontWeight: 'bold', fontSize: '14px' }} to={'/about-us'}>Learn More  <ArrowRightAltIcon /></Link>
-                    : <Button variant='contained' sx={{ backgroundColor: '#f5821f', fontSize: { xs: 20, md: 20, xl: 30 }, my: { xs: 2 }, padding: '10px 50px' }}>
-                        Get Started
-                    </Button>
-                }
-                {/* </motion.div> */}
+                <motion.div
+                    className="box"
+                    ref={ref}
+                    variants={boxVariant}
+                    initial="hidden"
+                    animate={control}
+                >
+                    <Typography variant="h3" color='#f58634' sx={{
+                        fontSize: { xs: 30, md: 36, lg: 40 },
+                    }}>
+                        {contentName}
+                    </Typography>
+                    <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 18 }, fontWeight: 300 }}>
+                        Welcome to Janak & Co., where dreams soar and brands conquer. We are a premier digital marketing agency committed to empowering businesses/brand to reach new heights of success in the ever-evolving online landscape. With a team of passionate experts driven by innovation, trust, bold strategies and confidence, we are committed to delivering outstanding results and empowering our clients to surpass their goals.
+                    </Typography>
+                    {order === false
+                        ? <Link style={{ fontWeight: 'bold', fontSize: '14px' }} to={'/about-us'}>Learn More  <ArrowRightAltIcon /></Link>
+                        : <Button variant='contained' sx={{ backgroundColor: '#f5821f', fontSize: { xs: 20, md: 20, xl: 30 }, my: { xs: 2 }, padding: '10px 50px' }}>
+                            Get Started
+                        </Button>
+                    }
+                </motion.div>
             </Grid>
         </Grid>
     )
--- src/features/WhyUs/why-us.component.tsx
+++ src/features/WhyUs/why-us.component.tsx
@@ -13,9 +13,19 @@ import Transparent from '../../assets/why choose us icon/Transparent Collaborati
 import Measurable from '../../assets/why choose us icon/Measurable Results.svg'
 import Value from '../../assets/why choose us icon/Value for Investment.svg'
 
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
 
 import whyPage from '../../assets/whyjc.svg'
 import AppCard from '../shared/components/Card';
+
+
+const boxVariant = {
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, scale: 0 }
+};
 
 export const Content = [
     {
@@ -65,34 +75,50 @@ export const Content = [
 
 
 export const WhyUsPageComponent = () => {
+
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            control.start("visible");
+        } else {
+            control.start("hidden");
+        }
+    }, [control, inView]);
+
     return (
         <Grid component='section' container sx={{ pl: { md: 2 }, minHeight: { xs: '100dvh', md: '85dvh', lg: '85dvh', xl: '10dvh' }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <Grid item xs={12} sm={6} lg={6} md={6} sx={{ order: { xs: 2, sm: 1 } }}>
-                {/* <motion.div
-                    initial={{ opacity: 0, x: 50 }} // Initial animation values
-                    animate={{ opacity: 1, x: 0 }} // Animation values to transition to
-                    transition={{ duration: 1.5 }} // Transition configuration
-                > */}
-                <Typography variant="h3" color='#f58634' sx={{
-                    fontSize: { xs: 28, md: 36, lg: 40 },
-                }}>
-                    Why chosse Janak & Co.?<br />
-                    Here what sets us apart
-                </Typography>
-                <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 18 }, fontWeight: 400 }}>
-                    At Janak & Co., we understand that choosing the right digital marketing agency is crucial for the success of your business. With numerous options available, its essential to partner with a team that not only delivers results but also aligns with your vision and values. We pride ourselves on being a trusted and reliable partner that goes above and beyond to help our clients soar above their competition. With our unwavering commitment to innovation, trust, and bold strategies, we are dedicated to transforming your digital presence and empowering your brand to conquer new horizons.
-                </Typography>
-                <Link to={'/why-us'} style={{ fontWeight: 'bold', fontSize: '16px' }}>Learn More<ArrowRightAltIcon /></Link>
-                {/* </motion.div> */}
+                <motion.div
+                    className="box"
+                    ref={ref}
+                    variants={boxVariant}
+                    initial="hidden"
+                    animate={control}
+                >
+                    <Typography variant="h3" color='#f58634' sx={{
+                        fontSize: { xs: 28, md: 36, lg: 40 },
+                    }}>
+                        Why chosse Janak & Co.?<br />
+                        Here what sets us apart
+                    </Typography>
+                    <Typography variant="h5" my={2} color="#746a6a" sx={{ fontSize: { xs: 16, md: 18 }, fontWeight: 400 }}>
+                        At Janak & Co., we understand that choosing the right digital marketing agency is crucial for the success of your business. With numerous options available, its essential to partner with a team that not only delivers results but also aligns with your vision and values. We pride ourselves on being a trusted and reliable partner that goes above and beyond to help our clients soar above their competition. With our unwavering commitment to innovation, trust, and bold strategies, we are dedicated to transforming your digital presence and empowering your brand to conquer new horizons.
+                    </Typography>
+                    <Link to={'/why-us'} style={{ fontWeight: 'bold', fontSize: '16px' }}>Learn More<ArrowRightAltIcon /></Link>
+                </motion.div>
             </Grid>
             <Grid item xs={12} sm={6} lg={6} md={6} sx={{ order: { xs: 1, sm: 2 } }}>
-                {/* <motion.div
-                    initial={{ opacity: 0, x: -50 }} // Initial animation values
-                    animate={{ opacity: 1, x: 0 }} // Animation values to transition to
-                    transition={{ duration: 1.5 }} // Transition configuration
-                > */}
-                <img src={whyPage} alt="whyChoseJanak&co" />
-                {/* </motion.div> */}
+                <motion.div
+                    className="box"
+                    ref={ref}
+                    variants={boxVariant}
+                    initial="hidden"
+                    animate={control}
+                >
+                    <img src={whyPage} alt="whyChoseJanak&co" />
+                </motion.div>
             </Grid>
         </Grid>
     )
